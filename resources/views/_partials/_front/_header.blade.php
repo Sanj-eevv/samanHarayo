1f92dc27116109110d535eddb1fd22a1177304e0
Sanjeev Bhandari <sanjeevvsanjeev1@gmail.com>
2021-11-27 03:46:03 +0545

    large header done

--- resources/views/_partials/_front/_header.blade.php
+++ resources/views/_partials/_front/_header.blade.php
@@ -27,7 +27,7 @@
                     <div class="col-xl-2 col-lg-3">
                         <div class="header-action header-action-flex">
                             <div class="same-style-2 header-search-1">
-                                <div class="search-wrap-1 open">
+                                <div class="search-wrap-1">
                                     <form action="#">
                                         <input placeholder="Search products…" type="text">
                                         <button class="button-search"><i class="icon-magnifier"></i></button>
