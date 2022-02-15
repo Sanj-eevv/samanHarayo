55a41ba6bbcbfc5cf1fcf6bc3584a66f1256b3b9
Sanjeev Bhandari <sanjeevvsanjeev1@gmail.com>
2022-02-15 06:47:39 +0545

    footer left

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,196 +70,89 @@
         </div>
     </div>
 </div>
-<div class="product-area section-padding-1 mt-70">
+<div class="product-area mt-70">
     <div class="container">
-        <div class="section-title-tab-wrap mb-45">
+        <div class="product-title-wrapper">
             <div class="section-title">
                 <h2>Featured Products</h2>
             </div>
-            <div class="tab-style-1 nav">
-                <a class="active" href="#product-1" data-bs-toggle="tab">Best Seller</a>
-                <a href="#product-2" data-bs-toggle="tab"> Trending</a>
+            <div class="section-nav">
+                <button class="active" href="#">Best Seller</button>
+                <button href="#"> Trending</button>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="tab-content jump">
-            <div id="product-1" class="tab-pane active">
-                <div class="all_product_container">
-                    <div class="single-product-wrap mb-35">
-                        <div class="product-img product-img-zoom mb-20">
-                            <a href="product-details.html">
-                                <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="">
-                            </a>
-                        </div>
-                        <div class="product-content-wrap">
-                            <div class="product-content-left">
-                                <h4><a href="product-details.html">Simple Black T-Shirt</a></h4>
-                                <div class="product-price">
-                                    <span>$56.20</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single-product-wrap mb-35">
-                        <div class="product-img product-img-zoom mb-20">
-                            <a href="product-details.html">
-                                <img src="assets/images/product/product-2.jpg" alt="">
-                            </a>
-                        </div>
-                        <div class="product-content-wrap">
-                            <div class="product-content-left">
-                                <h4><a href="product-details.html">Black Simple Sneaker</a></h4>
-                                <div class="product-price">
-                                    <span>$38.50</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                        <div class="single-product-wrap mb-35">
-                            <div class="product-img product-img-zoom mb-20">
-                                <a href="product-details.html">
-                                    <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="">
-                                </a>
-                                <span class="pro-badge left bg-red">-20%</span>
-                            </div>
-                            <div class="product-content-wrap">
-                                <div class="product-content-left">
-                                    <h4><a href="product-details.html">Lined Brown Swearshirt</a></h4>
-                                    <div class="product-price">
-                                        <span class="new-price">$46.00</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="single-product-wrap mb-35">
-                            <div class="product-img product-img-zoom mb-20">
-                                <a href="product-details.html">
-                                    <img src="{{asset('assets/logo.png')}}" alt="">
-                                </a>
-                            </div>
-                            <div class="product-content-wrap">
-                                <div class="product-content-left">
-                                    <h4><a href="product-details.html">Norda Simple Backpack</a></h4>
-                                    <div class="product-price">
-                                        <span>$102.54</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="single-product-wrap mb-35">
-                            <div class="product-img product-img-zoom mb-20">
-                                <a href="product-details.html">
-                                    <img src="assets/images/product/product-5.jpg" alt="">
-                                </a>
-                                <span class="pro-badge left bg-red">-15%</span>
-                            </div>
-                            <div class="product-content-wrap">
-                                <div class="product-content-left">
-                                    <h4><a href="product-details.html">Contrasting Sunglasses</a></h4>
-                                    <div class="product-price">
-                                        <span class="new-price">$35.45</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="single-product-wrap mb-35">
-                            <div class="product-img product-img-zoom mb-20">
-                                <a href="product-details.html">
-                                    <img src="assets/images/product/product-6.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="product-content-wrap">
-                                <div class="product-content-left">
-                                    <h4><a href="product-details.html">Simple Blue T-Shirt</a></h4>
-                                    <div class="product-price">
-                                        <span>$56.20</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="single-product-wrap mb-35">
-                            <div class="product-img product-img-zoom mb-20">
-                                <a href="product-details.html">
-                                    <img src="assets/images/product/product-7.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="product-content-wrap">
-                                <div class="product-content-left">
-                                    <h4><a href="product-details.html">Norda Sport Sneaker</a></h4>
-                                    <div class="product-price">
-                                        <span>$37.86</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="single-product-wrap mb-35">
-                            <div class="product-img product-img-zoom mb-20">
-                                <a href="product-details.html">
-                                    <img src="assets/images/product/product-8.jpg" alt="">
-                                </a>
-                                <span class="pro-badge left bg-black">Out of stock</span>
-                            </div>
-                            <div class="product-content-wrap">
-                                <div class="product-content-left">
-                                    <h4><a href="product-details.html">Basic Sneaker</a></h4>
-                                    <div class="product-price">
-                                        <span>$46.20</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+        <div class="all_product_container">
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
                 </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
             </div>
-            <div id="product-2" class="tab-pane">
-                <div class="all_product_container">
-                <div class="single-product-wrap mb-35">
-                    <div class="product-img product-img-zoom mb-20">
-                        <a href="product-details.html">
-                            <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="">
-                        </a>
-                        <span class="pro-badge left bg-red">-20%</span>
-                    </div>
-                    <div class="product-content-wrap">
-                        <div class="product-content-left">
-                            <h4><a href="product-details.html">Lined Brown Swearshirt</a></h4>
-                            <div class="product-price">
-                                <span class="new-price">$46.00</span>
-                            </div>
-                        </div>
-                    </div>
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
                 </div>
-
-
-                <div class="single-product-wrap mb-35">
-                    <div class="product-img product-img-zoom mb-20">
-                        <a href="product-details.html">
-                            <img src="{{asset('assets/logo.png')}}" alt="">
-                        </a>
-                    </div>
-                    <div class="product-content-wrap">
-                        <div class="product-content-left">
-                            <h4><a href="product-details.html">Norda Simple Backpack</a></h4>
-                            <div class="product-price">
-                                <span>$102.54</span>
-                            </div>
-                        </div>
-                    </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
+            </div>
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
                 </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
+            </div>
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
                 </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
+            </div>
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
+                </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
+            </div>
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
+                </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
+            </div>
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
+                </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
+            </div>
+            <div class="single-product-wrap">
+                <div class="product-img">
+                    <a href="#">
+                        <img src="{{asset('assets/images/users/avatar-1.jpg')}}" alt="" class="img-fluid">
+                    </a>
+                    <span class="pro-badge bg-red">-20%</span>
+                </div>
+                <h4><a href="#" class="underlined-link">Simple Black T-Shirt</a></h4>
             </div>
         </div>
     </div>
